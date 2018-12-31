dd9e80411e4e44c2673f1f1fec1d8f018672ed3d
chandrakumarreddy <chandrapenugonda655@gmail.com>
2018-12-31 22:44:04 +0530

    lesson 02 updated

---
+++ lesson-02/README.md
@@ -0,0 +1,28 @@
+//pure functions=> depends only on arguments passed.no interaction with outside code or with api's,databases etc
+
+function pureFunction(a, b, c) {
+return "a value is " + a + ",b value is " + b + ",c value is " + c;
+}
+
+console.log(pureFunction(2, 3, 4));
+
+//arrays and objects
+
+//array opertaions (not recommended to use in real world)
+data.push(3);
+data.pop();
+data.shift();
+data.unshift(0);
+
+//recommended to use in real world
+
+[1, 2, 3, 4].forEach(item => console.log(item));
+["red", "green", "blue"].find(item => item === "blue");
+[1, 2, 3].map(item => item \_ 2);
+[1, 2, 3].reduce((total, a) => total + a)
+(1, 2, 3)
+].filter(item => item > 1);
+
+//objects
+const obj = { a: 1, b: 2, c: 3 };
+obj.name = "chandra";
---
+++ lesson-02/index.js
@@ -0,0 +1,45 @@
+let name = "MAX"; //string
+let age = 21; //number
+let married = false; //boolean
+
+//pure functions=> depends only on arguments passed.no interaction with outside code or with api's,databases etc
+
+function pureFunction(a, b, c) {
+	return "a value is " + a + ",b value is " + b + ",c value is " + c;
+}
+
+console.log(pureFunction(2, 3, 4));
+
+//arrays and objects
+
+const data = [1, 2];
+
+//array opertaions (not recommended to use in real world)
+data.push(3);
+// console.log(data);
+data.pop();
+// console.log(data);
+data.shift();
+// console.log(data);
+data.unshift(0);
+// console.log(data);
+
+//recommended to use in real world
+
+[1, 2, 3, 4].forEach(item => console.log(item)); //output:- 1,2,3,4 loops through an array(limits the scope,easy to use and better readability)
+["red", "green", "blue"].find(item => item === "blue");
+//uncommet this to see output console.log(["red", "green", "blue"].find(item => item === "blue"));
+//output:- blue => use when single element is required
+[1, 2, 3].map(item => item * 2);
+// uncommet this to see output console.log([1, 2, 3].map(item => item * 2));
+// output:- [2,4,6] => use to transform the elements in an array
+[1, 2, 3].reduce((total, a) => total + a);
+// uncommet this to see outputconsole.log([1, 2, 3].reduce((total, a) => total + a));
+// 6 => use when you want single value from an elements in an array
+[1, 2, 3].filter(item => item > 1);
+// uncommet this to see output console.log([1, 2, 3].filter(item => item > 1));
+//[2,3] => use to select subset of an array
+
+//objects
+const obj = { a: 1, b: 2, c: 3 };
+obj.name = "chandra";
